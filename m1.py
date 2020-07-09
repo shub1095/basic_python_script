a30072e950ebcdb322868c0f6a65af19ce81bf27
Saket <saket@Sakets-MacBook-Pro-2.local>
2020-07-09 18:38:01 +0530

    adding python basic files

---
+++ m1.py
@@ -0,0 +1,14 @@
+#first python prog
+a=10
+b=3.05
+c=a+b
+
+list1=['python','java','c++']
+x=list1[0]
+
+print(c)
+print(x)
+
+print("Hello world!")
+print('Hello WORLD!')
+input('Presss Enter to end program.')
