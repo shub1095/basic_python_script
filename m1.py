a99604dc126538e1339065171a24f56c079fbd9f
Saket <saket@Sakets-MacBook-Pro-2.local>
2020-07-10 11:14:14 +0530

    added new file

--- m1.py
+++ m1.py
@@ -11,55 +11,55 @@ print(x)
 
 print("Hello world!")
 print('Hello WORLD!')
-input('Presss Enter to end program.')
+#input('Presss Enter to end program.')
 
 
 #strings
-'Shubham Gupta1'
-"Shubham Gupta2"
-'5'
-'5+4'
-'5'+'4'
+print('Shubham Gupta1')
+print("Shubham Gupta2")
+print('5')
+print('5+4')
+print('5'+'4')
 
-'doesn\'t'  
-"doesn't"
+print('doesn\'t')  
+print("doesn't")
 
-'"Yes,"he said1'
-'"Yes",he said2'
+print('"Yes,"he said1')
+print('"Yes",he said2')
 
-"\"hello! my name is john\" How are u"
+("\"hello! my name is john\" How are u")
 
 print('C:wonderful\newyork')     #new line interpreted
 print(r'C:wonderful\newyork')    #not new line interpreted
 
-'New'+'York'  #string concatenation
-5 * 'new'  #multiply
-'5' * '6'
-'new' * 'york'
-5 * 6
-5 * 'love' + 'python'
-5 * 'love' + 4 * 'python'
+print('New'+'York')  #string concatenation
+print(5 * 'new')  #multiply
+#print('5' * '6')
+#print('new' * 'york')
+print(5 * 6)
+print(5 * 'love' + 'python')
+print(5 * 'love' + 4 * 'python')
 
 word = 'Python'
 print(word[0])
 print(word[5])
-print(word[6])
+#print(word[6])
 print(word[-1])
 print(word[-6])
-print(word[-7])
+#print(word[-7])
 
 # triple quotes for multiple line of text within a string entry 
 # '''  or """
 # multiple line text entry
-string1 = ''' story or paragraph of python continues. Multiple
+string1 = '''story or paragraph of python continues. Multiple
 line of text entry into a string. 
-    cool feature.'''
+cool feature.'''
 print(string1)
 
 print('''\
 	i am going to narrate a great story.
-	chapter1: xyz
-	chapter2: abc
+	       chapter1: xyz
+	       chapter2: abc
 ''')
 
 
